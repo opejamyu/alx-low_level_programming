5f39f6230dfe6616ccc9e3c96bfee8bf7dce577a
opejamyu <opejamyu200@gmail.com>
2022-09-11 10:39:04 -0700

    mandatory task

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+	char low;
+
+	for (n = '0'; n <= '9'; n++)
+		putchar(n);
+	for (low = 'a'; low <= 'f'; low++)
+		putchar(low);
+	putchar('\n');
+
+	return (0);
+}
