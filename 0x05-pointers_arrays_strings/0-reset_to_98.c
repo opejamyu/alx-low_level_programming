6d1f0328a7d212d09179374617949138963caa72
opejamyu <opejamyu200@gmail.com>
2022-09-19 16:02:26 -0700

    1

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,11 +2,9 @@
 /**
  * reset_to_98 - takes the pointer and updates the value
  * @n: int to check
- * Owned By Bwave/Bright
  * Return: 0 is success
  */
-
-void reset_to_98(int *n);
+void reset_to_98(int *n)
 {
 	*n = 98;
 }
